f8baf1adaa99eefd91eca516ef9f0b8ca7212a61
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2023-08-11 19:27:17 -0600

    fix: halo2_lib example

--- snark-verifier-sdk/examples/halo2_lib.rs
+++ snark-verifier-sdk/examples/halo2_lib.rs
@@ -1,10 +1,15 @@
 use ark_std::{end_timer, start_timer};
+use halo2_base::gates::builder::{
+    BaseConfigParams, CircuitBuilderStage, GateThreadBuilder, RangeCircuitBuilder,
+    RangeWithInstanceCircuitBuilder, BASE_CONFIG_PARAMS,
+};
+use halo2_base::gates::flex_gate::GateStrategy;
 use halo2_base::gates::GateChip;
-use halo2_base::gates::builder::{CircuitBuilderStage, BASE_CONFIG_PARAMS, GateThreadBuilder, RangeCircuitBuilder, RangeWithInstanceCircuitBuilder};
 use halo2_base::halo2_proofs::halo2curves::bn256::Fr;
-use halo2_base::safe_types::{RangeChip, RangeInstructions, GateInstructions};
+use halo2_base::safe_types::{GateInstructions, RangeChip, RangeInstructions};
 use halo2_base::utils::fs::gen_srs;
 
+use snark_verifier_sdk::halo2::aggregation::VerifierUniversality;
 use snark_verifier_sdk::halo2::read_snark;
 use snark_verifier_sdk::SHPLONK;
 use snark_verifier_sdk::{
@@ -15,29 +20,36 @@ use snark_verifier_sdk::{
 use std::fs::File;
 use std::io::{BufReader, BufWriter};
 use std::path::Path;
-use snark_verifier_sdk::halo2::aggregation::VerifierUniversality;
 
 fn generate_circuit(k: u32) -> Snark {
     let mut builder = GateThreadBuilder::new(false);
     let ctx = builder.main(0);
-    let gate = GateChip::<Fr>::default();
     let range = RangeChip::<Fr>::default(8);
 
-    let x = builder.main(0).load_witness(Fr::from(14));
+    let x = ctx.load_witness(Fr::from(14));
     range.range_check(ctx, x, 64);
     range.gate().add(ctx, x, x);
 
     let circuit = RangeWithInstanceCircuitBuilder::<Fr>::keygen(builder.clone(), vec![]);
     let params = gen_srs(k);
 
+    BASE_CONFIG_PARAMS.with(|conf| {
+        *conf.borrow_mut() = BaseConfigParams {
+            strategy: GateStrategy::Vertical,
+            k: k as usize,
+            num_advice_per_phase: vec![1],
+            num_lookup_advice_per_phase: vec![1],
+            num_fixed: 1,
+            lookup_bits: Some(8),
+        };
+    });
+
     let pk = gen_pk(&params, &circuit, None);
     let breakpoints = circuit.break_points();
 
-    let circuit = RangeWithInstanceCircuitBuilder::<Fr>::prover(builder.clone(), vec![], breakpoints);
-    let snark = gen_snark_shplonk(&params, &pk, circuit, None::<&str>);
-    snark
-
-    
+    let circuit =
+        RangeWithInstanceCircuitBuilder::<Fr>::prover(builder.clone(), vec![], breakpoints);
+    gen_snark_shplonk(&params, &pk, circuit, None::<&str>)
 }
 
 fn gen_agg_break_points(agg_circuit: AggregationCircuit, path: &Path) -> Vec<Vec<usize>> {
@@ -63,34 +75,36 @@ fn main() {
     let dummy_snark = generate_circuit(13);
 
     let k = 14u32;
-    let params = gen_srs(k);
     let lookup_bits = k as usize - 1;
+    // this config is for aggregation circuit
     BASE_CONFIG_PARAMS.with(|config| {
         config.borrow_mut().lookup_bits = Some(lookup_bits);
         config.borrow_mut().k = k as usize;
     });
+    let params = gen_srs(k);
     let agg_circuit = AggregationCircuit::new::<SHPLONK>(
         CircuitBuilderStage::Keygen,
         None,
         lookup_bits,
         &params,
-        vec![dummy_snark.clone()],
+        vec![dummy_snark],
         VerifierUniversality::Full,
     );
     agg_circuit.config(k, Some(10));
+    let agg_config = BASE_CONFIG_PARAMS.with(|config| config.borrow().clone());
 
     let start0 = start_timer!(|| "gen vk & pk");
     let pk = gen_pk(&params, &agg_circuit, Some(Path::new("./examples/agg.pk")));
     end_timer!(start0);
     let break_points = gen_agg_break_points(agg_circuit, Path::new("./examples/break_points.json"));
 
-    let snarks = [
+    /*let snarks = [
         "./examples/halo2_lib_snarks/range.snark",
         "./examples/halo2_lib_snarks/halo2_lib.snark",
         "./examples/halo2_lib_snarks/poseidon.snark",
     ]
-    .map(|file| read_snark_from_file(file));
-    // let snarks = [dummy_snark];
+    .map(|file| read_snark(file).unwrap());*/
+    let snarks = [generate_circuit(14)];
     for (i, snark) in snarks.into_iter().enumerate() {
         let agg_circuit = AggregationCircuit::new::<SHPLONK>(
             CircuitBuilderStage::Prover,
@@ -100,6 +114,9 @@ fn main() {
             vec![snark],
             VerifierUniversality::Full,
         );
+        BASE_CONFIG_PARAMS.with(|config| {
+            *config.borrow_mut() = agg_config.clone();
+        });
         let _snark = gen_snark_shplonk(&params, &pk, agg_circuit, None::<&str>);
         println!("snark {i} success");
     }
