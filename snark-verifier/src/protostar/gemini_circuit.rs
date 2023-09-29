bc10eb3f005b3f3506f4395fe583f4dc320947a8
yuliakotel <yuliakotel@gmail.com>
2023-09-28 22:16:57 -0500

    gemini_circuit bugfix

--- snark-verifier/src/protostar/gemini_circuit.rs
+++ snark-verifier/src/protostar/gemini_circuit.rs
@@ -1,3 +1,6 @@
+#![allow(unused_imports)]
+#![allow(dead_code)]
+
 use std::{
     fmt::Debug,
     marker::PhantomData,
@@ -48,6 +51,7 @@ use crate::{
 use halo2_ecc::ecc::{EccChip, EcPoint};
 use num_bigint::{BigInt, BigUint};
 
+use halo2_base::utils::biguint_to_fe;
 
 const LIMBS: usize = 3;
 const BITS: usize = 88;
@@ -88,7 +92,7 @@ where
     fn evaluate_polynomial_at_a_point(
         &self,
         polynomial: Vec<ProperCrtUint<F>>,
-        point: ProperCrtUint<F>,
+        point: EcPoint<F, <FpChip<F, CF> as FieldChip<F>>::FieldPoint>,
     ) -> ProperCrtUint<F>;
         
 
@@ -156,7 +160,7 @@ impl <'range, F, CF, GA, L> GeminiChip<'range, F, CF, GA, L> for &EccChip<'range
     fn evaluate_polynomial_at_a_point(
         &self,
         polynomial: Vec<ProperCrtUint<F>>,
-        point: ProperCrtUint<F>,
+        point: EcPoint<F, <FpChip<F, CF> as FieldChip<F>>::FieldPoint>,
     ) -> ProperCrtUint<F>
         {
             unimplemented!()
@@ -180,11 +184,14 @@ impl <'range, F, CF, GA, L> GeminiChip<'range, F, CF, GA, L> for &EccChip<'range
         let zero = self.field_chip.load_constant(ctx, CF::zero());
 
         //define m_e = f_e(1), m_o = f_o(1)
-        let m_e = self.sum(ctx, polynomial_even);
-        let m_o = self.sum(ctx, polynomial_odd);
+        let m_even = polynomial_even.iter().map(|&&x| x.value()).sum::<BigUint>();
+        let m_odd = polynomial_odd.iter().map(|&&x| x.value()).sum::<BigUint>();
+
+        let m_even = self.field_chip.load_private(ctx, biguint_to_fe(&m_even));
+        let m_odd = self.field_chip.load_private(ctx, biguint_to_fe(&m_odd));
 
         // proof that m_e + m_o == target
-        self.sum_check(ctx, vec![m_o, m_e], target);
+        self.sum_check(ctx, vec![&m_even, &m_odd], target);
 
         // find poly'= m_e(X) + \challenge * m_o(X)
         let mut polynomial_new = vec![polynomial_even.iter().next().unwrap().to_owned().to_owned()];
@@ -192,11 +199,12 @@ impl <'range, F, CF, GA, L> GeminiChip<'range, F, CF, GA, L> for &EccChip<'range
         let polynomial_new_tail = 
                 polynomial_even.iter()
                                 .zip(polynomial_odd)
-                                .map(|(a, b)| self.scalar_mul_and_add_no_carry(ctx, b.to_owned(), a.to_owned().to_owned(), challenge));
+                                .map(|(a, b)| self.field_chip.scalar_mul_and_add_no_carry(ctx, b.to_owned(), a.to_owned().to_owned(), challenge));
 
         polynomial_new.extend(polynomial_new_tail);
 
-        let target_new = self.field_chip.add_no_carry(ctx, a, b);
+        //let target_new = self.field_chip.add_no_carry(ctx, );
+        let target_new = target;
 
 
         (polynomial_new, target_new)
@@ -216,8 +224,8 @@ impl <'range, F, CF, GA, L> GeminiChip<'range, F, CF, GA, L> for &EccChip<'range
         let degree = polynomial.len();
         while polynomial.len() > 1{
             //needs to be some transcript thing instead
-            let next_challenge = challenges.iter().max_by_keynext()
-            let curr = self.gemini_one_round(ctx, polynomial, next_challenge, target);
+            let next_challenge = challenges.iter().next().unwrap();
+            let curr = self.gemini_one_round(ctx, polynomial, &next_challenge, target);
             let polynomial = curr.0;
             let target = curr.1;
             //something like transcript.write_scalar(*curr.1);
